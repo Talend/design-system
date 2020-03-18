eb9d1748e0d37c7c4b322b68733023318b021521
Jimmy Somsanith <jimmy.somsanith@gmail.com>
2020-03-18 12:01:12 +0100

    Revert "Change color"
    This reverts commit 9b728989454eaf6e4ca5cee10aa09bb571f60e06.

--- src/tokens/colors.tokens.js
+++ src/tokens/colors.tokens.js
@@ -34,7 +34,7 @@ export default {
 	informationColor: palette.lochmara,
 	warningColor: palette.jaffa,
 	successColor: palette.rioGrande,
-	destructiveColor: 'red',
+	destructiveColor: palette.chesnutRose,
 	transparent: 'rgba(0,0,0,0)',
 	mainColor: greyscale.black,
 };
