a86693e776c2c631f60f06dd665c2a48b1404127
Fabien Rassinier <frassinier@talend.com>
2020-12-14 17:45:17 +0100

    fix: add layout tests

--- test-utils.js
+++ test-utils.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { ThemeProvider } from './src/components/ThemeProvider';
-import theme from './src/themes/light.theme';
+import ThemeProvider from './src/components/ThemeProvider';
 
 const AllTheProviders = ({ children }) => {
-	return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+	return <ThemeProvider>{children}</ThemeProvider>;
 };
 
 const customRender = (ui, options) => render(ui, { wrapper: AllTheProviders, ...options });
