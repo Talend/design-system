3db032b1cada7a58b60ca5a67b9eda23e704488b
Fabien Rassinier <frassinier@talend.com>
2021-01-06 18:04:42 +0100

    chore: ts

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Loading = React.forwardRef((props, ref) => (
+const Loading = React.forwardRef<React.ReactElement, React.PropsWithChildren<any>>((props, ref) => (
 	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" ref={ref} {...props}>
 		<g>
 			<path
