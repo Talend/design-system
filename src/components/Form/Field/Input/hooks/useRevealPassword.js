3f9d6e8d252f81c6238f1ccb86fd88175f4980eb
Fabien Rassinier <frassinier@talend.com>
2021-01-07 16:50:00 +0100

    fix: input type password

--- src/components/Form/Field/Input/hooks/useRevealPassword.js
+++ src/components/Form/Field/Input/hooks/useRevealPassword.js
@@ -15,14 +15,14 @@ export default function useRevealPassword() {
 				aria-hidden
 				{...props}
 			>
-				<Icon name={revealed ? 'eye' : 'eyeSlash'} />
+				<Icon name={revealed ? 'talend-eye' : 'talend-eye-slash'} />
 			</Button>
 		);
 	}
 
 	function onReveal(event) {
 		event.preventDefault();
-		setRevealed((prevState) => !prevState);
+		setRevealed(prevState => !prevState);
 	}
 
 	function onReset() {
