d1ad824302bab278fbac460772f24a0b9ad9971f
Fabien Rassinier <frassinier@talend.com>
2021-01-07 13:41:36 +0100

    feat: Popover

--- src/components/Popover/Popover.component.tsx
+++ src/components/Popover/Popover.component.tsx
@@ -8,15 +8,14 @@ export type PopoverProps = React.PropsWithChildren<any> & {
 };
 
 const Popover = ({ disclosure, ...props }: PopoverProps) => {
-	const popover = usePopoverState();
+	const popover = usePopoverState({ animated: 250 });
 	return (
 		<>
 			<S.PopoverDisclosure {...popover} ref={disclosure.ref} {...disclosure.props}>
 				{disclosureProps => React.cloneElement(disclosure, disclosureProps)}
 			</S.PopoverDisclosure>
 			<S.Popover {...popover} {...props}>
-				<S.PopoverArrow {...popover} />
-				{props.children}
+				<S.AnimatedPopover>{props.children}</S.AnimatedPopover>
 			</S.Popover>
 		</>
 	);
