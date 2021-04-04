13591176f25c1bbbba2501f384137e60bbafa30d
Fabien Rassinier <frassinier@talend.com>
2021-04-04 10:25:34 +0200

    doc(Storybook): GitHub link

--- src/docs/index.js
+++ src/docs/index.js
@@ -3,10 +3,23 @@ import Col from './Col.component';
 import ColorSwatches from './ColorSwatches.component';
 import FigmaImage from './FigmaImage';
 import FigmaLink from './FigmaLink';
+import GitHubLink from './GitHubLink';
 import Grid from './Grid.component';
 import Table from './Table.component';
 import Row from './Row.component';
 import { Use } from './Use.component';
 import { Icons } from './Icons.component';
 
-export { Card, Col, ColorSwatches, FigmaImage, FigmaLink, Icons, Grid, Table, Row, Use };
+export {
+	Card,
+	Col,
+	ColorSwatches,
+	FigmaImage,
+	FigmaLink,
+	GitHubLink,
+	Grid,
+	Icons,
+	Row,
+	Table,
+	Use,
+};
