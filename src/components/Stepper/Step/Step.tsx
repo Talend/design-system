b8d208ab3916199204d0bbaffaa1e34367bc067f
Nicolas Comont <ncomont@talend.com>
2021-04-12 11:13:14 +0200

    oops

--- src/components/Stepper/Step/Step.tsx
+++ src/components/Stepper/Step/Step.tsx
@@ -7,8 +7,6 @@ import * as S from './Step.style';
 export type StepProps = {
 	/** The title of the step */
 	title: string;
-	/** The index displayed in front of the step */
-	index: string;
 	/** The optionnal class name */
 	className?: string;
 	/** The icon element to display */
@@ -19,10 +17,9 @@ export type StepProps = {
  Steps are the main elements for the stepper.
  * */
 
-const Step: React.FC<StepProps> = ({ icon, index, title, className = '', ...rest }: StepProps) => {
+const Step: React.FC<StepProps> = ({ icon, title, className = '', ...rest }: StepProps) => {
 	return (
 		<S.Step role="status" aria-live="polite" {...rest} className={`step ${className || ''}`}>
-			{/* <span className="step__index">{index}</span> */}
 			<span className="step__title">{title}</span>
 			<span className="step__icon">{icon && <Icon name={icon} />}</span>
 		</S.Step>
