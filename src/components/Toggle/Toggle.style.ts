da13e5539084cd13562d6b5bdf34d7a33899e682
Fabien Rassinier <frassinier@talend.com>
2021-04-13 13:03:55 +0200

    feat(components): CSS Variables

--- src/components/Toggle/Toggle.style.ts
+++ src/components/Toggle/Toggle.style.ts
@@ -3,29 +3,29 @@ import Button from '../Button';
 
 export const Toggle = styled(Button.Icon)`
 	&.btn--is-active {
-		--button-color: ${({ theme }) => theme.colors?.buttonPrimaryColor};
-		--button-background-color: ${({ theme }) => theme.colors?.buttonPrimaryBackgroundColor};
-		--button-border-color: ${({ theme }) => theme.colors?.buttonPrimaryBackgroundColor};
+		--t-button-color: ${({ theme }) => theme.colors?.buttonPrimaryColor};
+		--t-button-background-color: ${({ theme }) => theme.colors?.buttonPrimaryBackgroundColor};
+		--t-button-border-color: ${({ theme }) => theme.colors?.buttonPrimaryBackgroundColor};
 
 		&:hover,
 		&:active {
-			--button-color: ${({ theme }) => theme.colors?.buttonPrimaryColor};
+			--t-button-color: ${({ theme }) => theme.colors?.buttonPrimaryColor};
 		}
 
 		&:hover {
-			--button-background-color: ${({ theme }) => theme.colors?.buttonPrimaryHoverBackgroundColor};
-			--button-border-color: ${({ theme }) => theme.colors?.buttonPrimaryHoverBackgroundColor};
+			--t-button-background-color: ${({ theme }) => theme.colors?.buttonPrimaryHoverBackgroundColor};
+			--t-button-border-color: ${({ theme }) => theme.colors?.buttonPrimaryHoverBackgroundColor};
 		}
 
 		&:active {
-			--button-background-color: ${({ theme }) => theme.colors?.buttonPrimaryActiveBackgroundColor};
-			--button-border-color: ${({ theme }) => theme.colors?.buttonPrimaryActiveBackgroundColor};
+			--t-button-background-color: ${({ theme }) => theme.colors?.buttonPrimaryActiveBackgroundColor};
+			--t-button-border-color: ${({ theme }) => theme.colors?.buttonPrimaryActiveBackgroundColor};
 		}
 
 		&[aria-disabled='true'] {
-			--button-color: ${({ theme }) => theme.colors?.buttonDisabledBackgroundColor};
-			--button-background-color: ${({ theme }) => theme.colors?.buttonDisabledColor};
-			--button-border-color: ${({ theme }) => theme.colors?.buttonDisabledColor};
+			--t-button-color: ${({ theme }) => theme.colors?.buttonDisabledBackgroundColor};
+			--t-button-background-color: ${({ theme }) => theme.colors?.buttonDisabledColor};
+			--t-button-border-color: ${({ theme }) => theme.colors?.buttonDisabledColor};
 		}
 	}
 `;
