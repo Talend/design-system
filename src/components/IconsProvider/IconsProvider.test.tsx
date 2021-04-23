bff0b6a3739869e5827e4830ae5e25e30b84c0b9
Romain GUILLAUME <rguillaume@talend.com>
2021-04-23 16:17:43 +0200

    chore: add tests

--- src/components/IconsProvider/IconsProvider.test.tsx
+++ src/components/IconsProvider/IconsProvider.test.tsx
@@ -5,7 +5,7 @@ import { IconsProvider } from './index';
 describe('IconsProvider', () => {
 	test('should match snapshot', () => {
 		const { container } = render(<IconsProvider />);
-		expect(container.firstChild).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
 	});
 
 	test('should prevent multiple instance creation', () => {
@@ -15,6 +15,38 @@ describe('IconsProvider', () => {
 				<IconsProvider />
 			</>
 		);
-		expect(container.firstChild).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
+	});
+
+	it('should render default custom icons defined on icons prop', () => {
+		const customIcons = {
+			custom: <svg />,
+		};
+		const { container } = render(<IconsProvider icons={customIcons} />);
+		const symbols = container.querySelectorAll('symbol');
+		expect(symbols.length).toBe(1);
+		expect(symbols[ 0 ].id).toBe('custom');
+	});
+
+	it('should support defaultIcons props', () => {
+		const defaultIcons = {
+			default: <svg id="OverrideDefaultIcon" />,
+		};
+		const { container } = render(<IconsProvider defaultIcons={defaultIcons} />);
+		const symbols = container.querySelectorAll('symbol');
+		expect(symbols.length).toBe(1);
+		expect(symbols[ 0 ].id).toBe('default');
+	});
+
+	it('should support defaultIcons props and render custom icon', () => {
+		const defaultIcons = {
+			default: <svg id="OverrideDefaultIcon" />,
+		};
+		const customIcons = {
+			custom: <svg id="customIcon" />,
+		};
+		const { container } = render(<IconsProvider defaultIcons={defaultIcons} icons={customIcons} />);
+		const symbols = container.querySelectorAll('symbol');
+		expect(symbols.length).toBe(2);
 	});
 });
