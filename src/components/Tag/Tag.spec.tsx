03c89e895cd2bd442a733ec2ea96a5f721a3e011
Fabien Rassinier <frassinier@talend.com>
2021-04-26 18:17:02 +0200

    chore(E2E): components testing with Cypress

--- src/components/Tag/Tag.spec.tsx
+++ src/components/Tag/Tag.spec.tsx
@@ -1,17 +1,11 @@
 /// <reference types="cypress" />
 import React from 'react';
-import { mount } from '@cypress/react';
 
 import Tag from './';
-import ThemeProvider from '../ThemeProvider';
-
-function mountWithTheme(children: React.ReactNode) {
-	return mount(<ThemeProvider>{children}</ThemeProvider>);
-}
 
 context('<Tag />', () => {
 	it('should render', () => {
-		mountWithTheme(<Tag>Lorem ipsum</Tag>);
+		cy.mount(<Tag>Lorem ipsum</Tag>);
 		cy.get('.tag').should('have.text', 'Lorem ipsum');
 	});
 });
