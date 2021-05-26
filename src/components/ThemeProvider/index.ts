a62dfa28bacec6666498374be2c2a047ec3f4b09
Fabien Rassinier <frassinier@talend.com>
2021-05-26 15:35:07 +0200

    feat(ThemeProvider): Embed fonts

--- src/components/ThemeProvider/index.ts
+++ src/components/ThemeProvider/index.ts
@@ -2,7 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 
 import ThemeSwitcher from './ThemeSwitcher';
 import ThemeProvider from './ThemeProvider';
-import useGoogleFont from './useGoogleFont';
 
 import { GlobalStyle } from './ThemeProvider.style';
 
@@ -10,12 +9,10 @@ const TalendThemeProvider = ThemeProvider as typeof ThemeProvider & {
 	createGlobalStyle: typeof createGlobalStyle;
 	GlobalStyle: typeof GlobalStyle;
 	ThemeSwitcher: typeof ThemeSwitcher;
-	useGoogleFont: typeof useGoogleFont;
 };
 
 TalendThemeProvider.createGlobalStyle = createGlobalStyle;
 TalendThemeProvider.GlobalStyle = GlobalStyle;
 TalendThemeProvider.ThemeSwitcher = ThemeSwitcher;
-TalendThemeProvider.useGoogleFont = useGoogleFont;
 
 export default TalendThemeProvider;
