512619b45892e3ac0093012074faa780356b3977
Fabien Rassinier <frassinier@talend.com>
2021-08-20 12:07:14 +0200

    chore(Tests): @storybook/testing-react

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { mount } from '@cypress/react';
-import '@applitools/eyes-cypress/commands';
 import { setGlobalConfig } from '@storybook/testing-react';
 import * as globalStorybookConfig from '../../.storybook/preview'; // path of your preview.js file
 
