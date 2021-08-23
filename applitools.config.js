21cef6c49503e76f2d95a9bba65d6b11f32248e6
Fabien <frassinier@talend.com>
2021-08-23 11:18:20 +0200

    add Batch ID [skip ci]

--- applitools.config.js
+++ applitools.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	testConcurrency: 10,
 	apiKey: process.env.APPLITOOLS_API_KEY,
+	batchId: process.env.APPLITOOLS_BATCH_ID.
 	batchName: 'Design System - Storybook',
 	showLogs: false,
 };
