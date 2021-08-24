9b068532b544cdda79e1d9e727889583bb673846
Fabien Rassinier <frassinier@talend.com>
2021-08-24 21:57:36 +0200

    chore(Tests): Applitools support

--- applitools.config.js
+++ applitools.config.js
@@ -1,13 +1,19 @@
 module.exports = {
-	testConcurrency: 10,
+	accessibilityValidation: { level: 'AA', guidelinesVersion: 'WCAG_2_0' },
 	apiKey: process.env.APPLITOOLS_API_KEY,
 	batchId: process.env.APPLITOOLS_BATCH_ID,
 	batchName: 'Design System - Storybook',
 	browser: [
 		{ width: 1366, height: 768, name: 'chrome' },
+		{ width: 1366, height: 768, name: 'chrome-one-version-back' },
 		{ width: 1366, height: 768, name: 'safari' },
-		{ deviceName: 'ipad' },
+		{ width: 1366, height: 768, name: 'safari-one-version-back' },
+		{ width: 1366, height: 768, name: 'firefox' },
+		{ width: 1366, height: 768, name: 'firefox-one-version-back' },
+		{ width: 1366, height: 768, name: 'edgechromium' },
+		{ width: 1366, height: 768, name: 'edgechromium-one-version-back' },
 	],
 	showLogs: false,
+	testConcurrency: 10,
 	waitBeforeScreenshot: 250,
 };
