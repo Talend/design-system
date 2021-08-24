cc3795836023a7c176574f29d5c1e120054fbe6e
Fabien Rassinier <frassinier@talend.com>
2021-08-24 21:30:38 +0200

    chore(Tests): Applitools support

--- applitools.config.js
+++ applitools.config.js
@@ -3,6 +3,11 @@ module.exports = {
 	apiKey: process.env.APPLITOOLS_API_KEY,
 	batchId: process.env.APPLITOOLS_BATCH_ID,
 	batchName: 'Design System - Storybook',
+	browser: [
+		{ width: 1366, height: 768, name: 'chrome' },
+		{ width: 1366, height: 768, name: 'safari' },
+		{ deviceName: 'ipad' },
+	],
 	showLogs: false,
 	waitBeforeScreenshot: 250,
 };
