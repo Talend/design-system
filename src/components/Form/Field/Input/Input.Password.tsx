3d9fbf768804d75a68a8958e6a95932fa562dcf8
Benrajalu <rajalubenoit@gmail.com>
2021-10-13 09:46:06 +0200

    wip

--- src/components/Form/Field/Input/Input.Password.tsx
+++ src/components/Form/Field/Input/Input.Password.tsx
@@ -6,15 +6,17 @@ import useRevealPassword from './hooks/useRevealPassword';
 const Password = React.forwardRef((props: InputProps, ref: React.Ref<HTMLInputElement>) => {
 	const { currentType, onReset, RevealPasswordButton } = useRevealPassword();
 	const inputRef = useRef<HTMLInputElement | null>(null);
-	const fromClickRef = useRef<boolean>(false);
 
-	React.useEffect(() => {
-		if (fromClickRef.current) {
-			inputRef.current?.focus();
-			const valueLength = inputRef.current?.value.length || 0;
-			inputRef.current?.setSelectionRange(valueLength, valueLength);
-		}
-	}, [currentType]);
+	const handleClick = React.useCallback(
+		function handleClick() {
+			if (inputRef.current) {
+				inputRef.current.focus();
+				const valueLength = inputRef.current.value.length || 0;
+				inputRef.current.setSelectionRange(valueLength, valueLength);
+			}
+		},
+		[inputRef],
+	);
 
 	return (
 		<div ref={ref}>
@@ -23,20 +25,12 @@ const Password = React.forwardRef((props: InputProps, ref: React.Ref<HTMLInputEl
 				type={currentType}
 				ref={inputRef}
 				onBlur={(event: React.FocusEvent<HTMLInputElement>) => {
-					fromClickRef.current = false;
 					onReset();
 					if (props.onBlur) {
 						props.onBlur(event);
 					}
 				}}
-				// @ts-ignore
-				after={
-					<RevealPasswordButton
-						onClick={() => {
-							fromClickRef.current = true;
-						}}
-					/>
-				}
+				after={<RevealPasswordButton onClick={() => handleClick()} disabled={props.disabled} />}
 			/>
 		</div>
 	);
