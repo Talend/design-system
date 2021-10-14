31675f3d00c8fe585d5e144875aeefdb004b7f73
Fabien Rassinier <frassinier@talend.com>
2021-10-14 17:53:07 +0200

    fix(Components/Form/Password): autoFocus

--- src/components/Form/Field/Input/Password/Password.tsx
+++ src/components/Form/Field/Input/Password/Password.tsx
@@ -1,22 +1,19 @@
 import React, { useRef } from 'react';
-import Input, { InputProps } from './Input';
+import Input, { InputProps } from '../Input';
 
-import useRevealPassword from './hooks/useRevealPassword';
+import useRevealPassword from '../hooks/useRevealPassword';
 
 const Password = React.forwardRef((props: InputProps, ref: React.Ref<HTMLInputElement>) => {
 	const { currentType, onReset, RevealPasswordButton } = useRevealPassword();
 	const inputRef = useRef<HTMLInputElement | null>(null);
 
-	const handleClick = React.useCallback(
-		function handleClick() {
-			if (inputRef.current) {
-				inputRef.current.focus();
-				const valueLength = inputRef.current.value.length || 0;
-				inputRef.current.setSelectionRange(valueLength, valueLength);
-			}
-		},
-		[inputRef],
-	);
+	function handleClick() {
+		if (inputRef.current) {
+			inputRef.current.focus();
+			const valueLength = inputRef.current.value.length || 0;
+			inputRef.current.setSelectionRange(valueLength, valueLength);
+		}
+	}
 
 	return (
 		<div ref={ref}>
