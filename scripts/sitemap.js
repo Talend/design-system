56fdbcbe3f0e1b6cb55f8d09d5f667366cc9e51e
Fabien Rassinier <frassinier@talend.com>
2021-10-20 23:22:47 +0200

    chore(Storybook): Algolia support

---
+++ scripts/sitemap.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+try {
+	const json = fs.readFileSync('./storybook-static/stories.json');
+	const data = JSON.parse(json);
+	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${Array.from(
+	new Set(
+		Object.values(data.stories).map(
+			({ id }) => `  <url>
+    <loc>https://design.talend.com/?path=/docs/${id.split('--')[0]}</loc>
+    <lastmod>2021-10-06</lastmod>
+  </url>`,
+		),
+	),
+).join('\n')}
+</urlset>`;
+
+	fs.writeFile('./static/sitemap.xml', sitemap, err => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+	});
+} catch (e) {
+	console.error(
+		'The file storybook-static/stories.json is not found! You should run `yarn extract-storybook` instead.',
+	);
+}
