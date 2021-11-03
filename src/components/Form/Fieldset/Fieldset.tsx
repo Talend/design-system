407d6920c75a28d2098e6631a45ce7e6a0c299b3
gjou-tlnd <58977230+gjou-tlnd@users.noreply.github.com>
2021-11-03 11:28:25 +0100

    Update src/components/Form/Fieldset/Fieldset.tsx
    Co-authored-by: Benrajalu <rajalubenoit@gmail.com>

--- src/components/Form/Fieldset/Fieldset.tsx
+++ src/components/Form/Fieldset/Fieldset.tsx
@@ -22,7 +22,7 @@ const Fieldset = React.forwardRef(
 						{required && '*'}
 					</S.Legend>
 				)}
-				{React.Children.toArray(children).map((child, key) =>
+				{React.Children.toArray(children).map((child) =>
 					isElement(child) ? React.cloneElement(child, childrenProps) : child,
 				)}
 			</S.Fieldset>
