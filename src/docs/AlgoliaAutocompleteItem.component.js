af75f5ca36315b0c12d223cb5c216f8e49123024
Fabien Rassinier <frassinier@talend.com>
2021-11-12 21:39:00 +0100

    chore(Storybook): Algolia support

--- src/docs/AlgoliaAutocompleteItem.component.js
+++ src/docs/AlgoliaAutocompleteItem.component.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 export function Item({ hit, components }) {
 	return (
-		<a href={hit.url} target="_parent" className="aa-ItemLink">
+		<a
+			href={hit.url?.replace(/iframe.html\?id=(.*)&/, '?path=/docs/$1&')}
+			target="_parent"
+			className="aa-ItemLink"
+		>
 			<div className="aa-ItemContent">
-				<div className="aa-ItemTitle">
-					<div>
-						<strong>{hit.title}</strong>
+				<div className="aa-ItemContentBody">
+					<div className="aa-ItemContentTitle">
+						<components.Snippet hit={hit} attribute="title" />
 					</div>
-					<components.Highlight hit={hit} attribute="content" />
+					<div className="aa-ItemContentDescription">
+						<components.Snippet hit={hit} attribute="content" />
+					</div>
 				</div>
 			</div>
 		</a>
